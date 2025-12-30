b586d9cb3dd7fa03cb0817219fe091cbefea227a
Apoorva reddy <kuntaapoorva62@gmail.com>
2025-12-30 23:31:14 +0530

    React Application

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Sample from "./Components/Sample"
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Deploying react application</h1>
+      <Sample/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Sample.jsx
@@ -0,0 +1,13 @@
+ export default function Sample(){
+    return(
+        <>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+             Minus inventore error sunt
+             laudantium in, assumenda voluptatibus 
+            nesciunt ex iure itaque explicabo architecto,
+             odio exercitationem voluptatem recusandae 
+             cupiditate vel reprehenderit nulla.</p>
+<img src="Dogs.jpg"></img>
+             </>
+    )
+}
